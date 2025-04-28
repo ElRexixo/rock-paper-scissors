929797aa6d7b411f9cb45f9c2d0c0b8cd02b9d35
ElRexixo <lordhomola@gmail.com>
2025-04-28 15:56:42 +0200

    Added random computer choice

---
+++ rps.js
@@ -0,0 +1,15 @@
+/* Random computer choice of either rock, paper or scissors*/
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+
+    if (randomNumber < 0.33) {
+        return "rock";
+    }   else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice())
+
