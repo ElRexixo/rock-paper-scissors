e526263ab6591046b9d73ff08a7fa42809f56f4e
ElRexixo <lordhomola@gmail.com>
2025-04-28 16:24:57 +0200

    added logic for single round of game

--- rps.js
+++ rps.js
@@ -1,17 +1,17 @@
 /* Random computer choice of either rock, paper or scissors*/
 
 function getComputerChoice() {
-    let randomNumber = Math.random();
+    let computerChoice = Math.random();
 
-    if (randomNumber < 0.33) {
+    if (computerChoice < 0.33) {
         return "rock";
-    }   else if (randomNumber < 0.66) {
+    }   else if (computerChoice < 0.66) {
         return "paper";
     } else {
         return "scissors";
     }
 }
-console.log(getComputerChoice())
+/*console.log(getComputerChoice())*/
 
 /* Get input from user*/
 
@@ -19,4 +19,33 @@ function getHumanChoice() {
     let humanChoice = prompt("rock, paper or scissors?")
     return humanChoice
 }
-console.log(getHumanChoice())
+/*console.log(getHumanChoice())*/
+
+/* Keeping score of the game */
+let humanScore = 0
+let computerScore = 0
+
+/* Single round of game */
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
